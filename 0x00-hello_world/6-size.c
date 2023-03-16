93d544c8b9d55ec5f77fbabe77c2f99eb479c0dc
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-16 04:53:47 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)\n",(int) sizeof(char));
+	printf("Size of a char: %ld byte(s)\n", (int) sizeof(char));
 	printf("Size of a int: %ld byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", (int) sizeof(long long int));
