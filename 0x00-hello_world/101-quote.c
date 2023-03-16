a604927788c90427612906d62dac7471e9c0f321
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-16 05:30:05 -0700

    01-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: all the time return 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
