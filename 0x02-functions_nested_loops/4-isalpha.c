a86a11097bbc3d8cdfe9d537b0a3f1bef80c5ef7
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-21 14:14:37 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _isalpha - Returns 1 if c is a letter and Returns 0 otherwise
+ *@c:the character to be checked
+ * Return: return in integer
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
