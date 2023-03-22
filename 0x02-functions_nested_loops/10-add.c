cef55bf254c50a4c32aa277f4059724de8d9d2b5
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-22 02:55:20 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - the function to add two numbers
+ * @n: number 1
+ * @m: number 2
+ * Return: return the result
+ */
+int add(int n, int m)
+{
+	int y;
+
+	y = n + m;
+	return (y);
+}
