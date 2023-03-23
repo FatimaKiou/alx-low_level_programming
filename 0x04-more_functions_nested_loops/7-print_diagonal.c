cc7a40ffa5b3b66128ac493740ba25c2dd760d84
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-23 06:48:19 -0700

    7-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,7 +9,7 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0 ; i <= n ; i++)
+		for (i = 0 ; i < n ; i++)
 		{
 			for (j = 0 ; j < n ; j++)
 			{
@@ -24,7 +24,6 @@ void print_diagonal(int n)
 			}
 			_putchar('\n');
 		}
-		n--;
 	}
 	else
 	{
