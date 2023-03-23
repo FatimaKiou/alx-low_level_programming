ce48d7e37b8bf222d67770bf267da0237e10f3d0
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-23 02:58:18 -0700

    0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - the function
+ * @c: the value of the function
+ * Return: return the result
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
