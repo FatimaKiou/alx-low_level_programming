5276ce2d2fb3d2b5f9a82e77543580526a5cf6e6
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-23 07:22:17 -0700

    8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - the function
+ * @size: the size
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int n, m;
+
+		for (n = 0 ; n < size ; n++)
+		{
+			for (m = 0 ; m <= size ; m++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
