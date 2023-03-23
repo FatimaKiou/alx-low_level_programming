da2622f05f26ffe31bcf8f0864a2d703e7bfa7b2
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-23 06:39:41 -0700

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_diagonal - the function
+ * @n: the int
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0 ; i <= n ; i++)
+		{
+			for (j = 0 ; j < n ; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+		n--;
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
