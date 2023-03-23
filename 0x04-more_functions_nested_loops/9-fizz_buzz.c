f41e1c9076cd1525395b81093f1c9d1fb72d80d5
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-23 07:49:02 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: return 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1 ; n < 100 ; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
