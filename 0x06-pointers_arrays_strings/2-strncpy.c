67fc2ea3ebc71f7f319e9a07cbc3a68e972016f9
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-29 05:18:05 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - the function
+ * @dest: dest
+ * @src: src
+ * @n: maximum number of characters to copy
+ * Return: the result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
