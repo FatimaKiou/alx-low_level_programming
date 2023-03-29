c4839c6ebf3b2beae1a6aaddb6272bf612b40e78
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-29 07:21:53 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - string to upper
+ * @str: the string
+ * Return: the result
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
