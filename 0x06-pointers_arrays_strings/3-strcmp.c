86bbb93359b840c8c9549136b1ac724519b064bd
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-03-29 05:41:05 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first char
+ * @s2: second char
+ * Return: the result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
+	if (s1[i] == s2[i])
+	{
+		return (0);
+	}
+	else if (s1[i] < s2[i])
+	{
+		return (-15);
+	}
+	else
+	{
+		return (15);
+	}
+}
