a7cae83d6d1b0b102dfeacb4b441c4d83b6a4b54
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-04-09 13:18:48 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the main function
+ * @argc: the first agrument
+ * @argv: the second agrument
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
