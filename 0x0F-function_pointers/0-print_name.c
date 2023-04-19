ce1c71d01fce5490403d62d2913c895522145407
FatimaKiou <fatima.kiouach@usmba.ac.ma>
2023-04-19 02:41:58 -0700

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - function
+ * @name: name
+ * @f: pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
